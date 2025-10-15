c89b8a15f3c139396564752e1615e4076544296d
DenizShayan <92824050+DenizShayan@users.noreply.github.com>
2025-10-15 13:51:24 -0400

    little change in app\page.tsx

--- nextjs-practice-app/src/app/page.tsx
+++ nextjs-practice-app/src/app/page.tsx
@@ -3,7 +3,7 @@ import Navbar from "../components/Navbar";
 export default function Home() {
   return (
     <div>
-      <Navbar />
+      {/* <Navbar /> */}
       <main className="p-4">
         <h1 className="text-2xl font-bold">Welcome to my Next.js App!</h1>
       </main>
