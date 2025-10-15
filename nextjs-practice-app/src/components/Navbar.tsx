47948a0077288741aed7aaa475d5b870f1d49a83
DenizShayan <92824050+DenizShayan@users.noreply.github.com>
2025-10-15 00:59:24 -0400

    Update Navbar.tsx

--- nextjs-practice-app/src/components/Navbar.tsx
+++ nextjs-practice-app/src/components/Navbar.tsx
@@ -3,28 +3,28 @@ import Link from 'next/link';
 export default function Navbar() {
     return (
         <nav className="bg-gray-800 p-4">
-        <ul className="flex space-x-4">
-            <li>
-            <Link href="/" className="text-white hover:text-gray-400">
-                Home
-            </Link>
-            </li>
-            <li>
-            <Link href="/about" className="text-white hover:text-gray-400">
-                About
-            </Link>
-            </li>
-            <li>
-            <Link href="/contact" className="text-white hover:text-gray-400">
-                Contact
-            </Link>
-            </li>
-        </ul>
+            <ul className="flex space-x-4">
+                <li>
+                    <Link href="/" className="text-white hover:text-gray-400">
+                        Home
+                    </Link>
+                </li>
+                <li>
+                    <Link href="/about" className="text-white hover:text-gray-400">
+                        About
+                    </Link>
+                </li>
+                <li>
+                    <Link href="/contact" className="text-white hover:text-gray-400">
+                        Contact
+                    </Link>
+                </li>
+            </ul>
 
-        <button className="ml-auto bg-blue-500 text-white px-3 py-1 rounded">
-          Add Task
-        </button>
-        
+            <button className="ml-auto bg-blue-500 text-white px-3 py-1 rounded">
+                Add Task
+            </button>
+
         </nav>
     );
-    }
+}
